614de6ef6b26236f70aca0440a20df841c07dea3
carson <carson@leaddyno.com>
2020-09-04 12:08:09 -0600

    more demo stuff

--- examples/select.sql
+++ examples/select.sql
@@ -15,11 +15,23 @@ SELECT name
 FROM tracks
 WHERE Milliseconds > 3 * 60 * 1000;
 
-SELECT name
+SELECT name, AlbumId
 FROM tracks
 WHERE AlbumId = 1;
 
-SELECT name
+SELECT Title
+FROM albums
+WHERE   AlbumId = 1;
+
+SELECT name, Milliseconds, AlbumId
 FROM tracks
-WHERE AlbumId = 1 AND Milliseconds > 3 * 60 * 1000;
+WHERE AlbumId = 1 OR Milliseconds > 3 * 60 * 1000;
+
+SELECT name, Milliseconds, AlbumId
+FROM tracks
+WHERE NOT AlbumId = 1 AND Milliseconds > 3 * 60 * 1000;
+
+select Name from tracks where Composer IS NULL;
+
+
 
