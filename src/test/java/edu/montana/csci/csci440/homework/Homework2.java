155fb3dc588b06802f35726a468cf72eec9d7c92
carson <carson@leaddyno.com>
2020-09-14 12:13:57 -0600

    fix hwk 2

--- src/test/java/edu/montana/csci/csci440/homework/Homework2.java
+++ src/test/java/edu/montana/csci/csci440/homework/Homework2.java
@@ -40,15 +40,14 @@ public class Homework2 extends DBTest {
                 "SELECT tracks.Name as TrackName, albums.Title as AlbumTitle, artists.Name as ArtistsName FROM tracks " +
                         "-- NEED TO DO SOME JOINS HERE KIDS");
 
-        System.out.println(results);
-        assertEquals(260, results.size());
-        assertEquals("Sleeping Village", results.get(0).get("TrackName"));
-        assertEquals("Black Sabbath", results.get(0).get("AlbumTitle"));
-        assertEquals("Black Sabbath", results.get(0).get("ArtistsName"));
+        assertEquals(623, results.size());
+        assertEquals("Princess of the Dawn", results.get(0).get("TrackName"));
+        assertEquals("Restless and Wild", results.get(0).get("AlbumTitle"));
+        assertEquals("Accept", results.get(0).get("ArtistsName"));
 
-        assertEquals("Walkin'", results.get(10).get("TrackName"));
-        assertEquals("The Essential Miles Davis [Disc 1]", results.get(10).get("AlbumTitle"));
-        assertEquals("Miles Davis", results.get(10).get("ArtistsName"));
+        assertEquals("Snoopy's search-Red baron", results.get(10).get("TrackName"));
+        assertEquals("The Best Of Billy Cobham", results.get(10).get("AlbumTitle"));
+        assertEquals("Billy Cobham", results.get(10).get("ArtistsName"));
 
     }
 
