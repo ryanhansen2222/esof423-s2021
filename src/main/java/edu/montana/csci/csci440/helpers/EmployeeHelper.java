4e3a953dddee335d5a2f030e318d81ef0153c4b0
carson <carson@leaddyno.com>
2020-09-28 09:17:10 -0600

    move impl out of controller so it is testable

--- src/main/java/edu/montana/csci/csci440/helpers/EmployeeHelper.java
+++ src/main/java/edu/montana/csci/csci440/helpers/EmployeeHelper.java
@@ -2,11 +2,16 @@ package edu.montana.csci.csci440.helpers;
 
 import edu.montana.csci.csci440.model.Employee;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class EmployeeHelper {
-    public static String makeTreeRoot(Employee employee, Map<Long, List<Employee>> employeeMap) {
+    public static String makeEmployeeTree() {
+        // TODO, change this to use a single query operation to get all employees
+        Employee employee = Employee.find(1); // root employee
+        // and use this data structure to maintain reference information needed to build the tree structure
+        Map<Long, List<Employee>> employeeMap = new HashMap<>();
         return "<ul>" + makeTree(employee, employeeMap) + "</ul>";
     }
 
