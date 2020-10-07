327fa4d162ddafe074af2c97451062b26025e9e5
carson <carson@leaddyno.com>
2020-10-06 19:58:33 -0600

    add equals/hash implementation to model so we can avoid implementing it

--- src/main/java/edu/montana/csci/csci440/model/Model.java
+++ src/main/java/edu/montana/csci/csci440/model/Model.java
@@ -1,8 +1,10 @@
 package edu.montana.csci.csci440.model;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Objects;
 
 // base class for entities
 public class Model {
@@ -37,4 +39,40 @@ public class Model {
         return _errors.size() > 0;
     }
 
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return  false;
+        }
+        if (obj.getClass() != this.getClass()) {
+            return false;
+        }
+        Field[] declaredFields = this.getClass().getDeclaredFields();
+        for (Field declaredField : declaredFields) {
+            try {
+                if (!declaredField.get(this).equals(declaredField.get(obj))) {
+                    return false;
+                }
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        Field[] declaredFields = this.getClass().getDeclaredFields();
+        List<Object> values = new LinkedList<>();
+        for (Field declaredField : declaredFields) {
+            try {
+                values.add(declaredField.get(this));
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+                return 0;
+            }
+        }
+        return Objects.hash(values.toArray());
+    }
 }
