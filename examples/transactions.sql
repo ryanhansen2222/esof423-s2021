c6094db4c03bcf4388a4428b3fd2d79d5e5c0767
carson <carson@leaddyno.com>
2020-10-22 09:56:22 -0600

    temp work

--- examples/transactions.sql
+++ examples/transactions.sql
@@ -13,13 +13,15 @@ UPDATE tracks
 SET Bytes=(Bytes + 10)
 WHERE TrackId = 2;
 
-
+ALTER TABLE albums ADD COLUMN Bytes INTEGER;
 
 BEGIN TRANSACTION;
 
-UPDATE tracks
-SET Bytes=(Bytes - 10)
-WHERE TrackId = 1;
+UPDATE albums
+SET Bytes=(SELECT SUM(Bytes) FROM tracks WHERE tracks.AlbumId = 1)
+WHERE AlbumId = 1;
+
+COMMIT;
 
 UPDATE tracks
 SET Bytes=(Bytes + 10)
