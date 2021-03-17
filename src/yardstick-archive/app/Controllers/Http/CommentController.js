e9e1631f87ab32ffc46aec040ed1c2dcee4313e7
Ryan <ryanhansen2222@gmail.com>
2021-03-16 19:05:23 -0600

    Comments basically done

--- src/yardstick-archive/app/Controllers/Http/CommentController.js
+++ src/yardstick-archive/app/Controllers/Http/CommentController.js
@@ -1,22 +1,53 @@
 'use strict'
 
 const Comment = use('App/Models/Comment')
+const Database = use('Database')
+const User = use('App/Models/User')
 
 class CommentController {
   async home({view}) {
-    //create a comments
+
+
+    //create user
+    const test = ((await Database.from('users').getMax('id'))+1).toString();
+    const user = new User;
+    user.username = ('George' + test);
+    user.email = ('fakemail' + test + '@test.com');
+    user.password = 'trollslyfe';
+
+    await user.save();
+
+    //create a comment
     const comment = new Comment;
-    comment.text = 'Trollolol';
+    comment.text = 'Dynamic Comment Testing BABYYYY';
     comment.video_id = '1';
-    comment.user_id = '2';
+    comment.user_id = test;
 
     await comment.save();
 
-    // Fetch all comment
+    // Fetch all comments
     const comments = await Comment.all();
 
+    //Find Username + Video Name
+    //name = await Database
+    var rawjson = comments.toJSON();
+    var packagedjson = rawjson;
 
-    return view.render('index', {comments: comments.toJSON() });
+
+    for(let comm in rawjson){
+        var username = await Database.table('users').where('id', rawjson[comm].id).pluck('username');
+
+        packagedjson[comm].username = username;
+
+    }
+
+
+    const dict = {comments: packagedjson};
+
+
+
+
+    return view.render('index', dict);
   }
 }
 
