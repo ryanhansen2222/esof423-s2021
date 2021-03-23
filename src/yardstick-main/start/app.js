3df71934ff7bdfab2c75bc36cbb56dffabab3cbb
Alan Tong <alantong1068@gmail.com>
2021-03-22 18:19:17 -0600

    transition from archive to main

--- src/yardstick-main/start/app.js
+++ src/yardstick-main/start/app.js
@@ -18,7 +18,8 @@ const providers = [
   '@adonisjs/cors/providers/CorsProvider',
   '@adonisjs/shield/providers/ShieldProvider',
   '@adonisjs/session/providers/SessionProvider',
-  '@adonisjs/auth/providers/AuthProvider'
+  '@adonisjs/auth/providers/AuthProvider',
+  '@adonisjs/validator/providers/ValidatorProvider'
 ]
 
 /*
@@ -32,6 +33,7 @@ const providers = [
 */
 const aceProviders = [
   '@adonisjs/lucid/providers/MigrationsProvider'
+  
 ]
 
 /*
